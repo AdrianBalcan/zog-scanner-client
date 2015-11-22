c6b4e049116767d7b45cb9bf95c4398dd2a50a1b
Adrian Balcan <adynett@gmail.com>
2015-11-22 19:00:08 +0000

    Client Scanner

---
+++ client.js
@@ -0,0 +1,26 @@
+var io = require('socket.io-client');
+var os = require("os");
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+client = io.connect('http://localhost:8080');
+var data = '';
+
+client.on('connect',function() {
+    console.log('Connected');
+    client.emit('adduser','os.hostname()');
+}); 
+client.on('disconnect',function() {
+    console.log('Disconnected');
+}); 
+process.stdin.on('data', function (text) {
+    data = text.replace(/\n|\r/g, "");
+    console.log('CID: ', data);
+    if (/^\d+\n$/.test(text)){
+      client.emit('cid',data);
+    } else {
+      client.emit('cid',0);
+    } 
+    if (text === 'quit\n') {
+      process.exit();
+    }
+  });
